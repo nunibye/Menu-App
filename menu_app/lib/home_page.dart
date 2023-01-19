3f5e8e7aeb67570387012597e253fc97e27595ea
christianknab <christiantknab@gmail.com>
2023-01-18 18:17:06 -0800

    added scrollable list

--- menu_app/lib/home_page.dart
+++ menu_app/lib/home_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:menu_app/cowell_menu.dart';
 
-
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
@@ -20,86 +19,158 @@ class HomePage extends StatelessWidget {
       //   child: const Text("Cowell"),
       // ),
 
-      body: Row(
-        mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.center,
+      // FIGURE OUT HOW TO CENTER THE SCROLLABLE LIST
+      body: ListView(
+        scrollDirection: Axis.horizontal,
         children: [
-          Column(
-            children: [
-              IconButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(builder: (BuildContext context) {
-                      return const CowellMenu();
-                    }),
-                  );
-                },
-                icon: Image.asset('images/cowell.png'),
-                iconSize: hallSize,
-              ),
-              IconButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(builder: (BuildContext context) {
-                      return const CowellMenu();
-                    }),
-                  );
-                },
-                icon: Image.asset('images/porter.png'),
-                iconSize: hallSize,
-              ),
-              IconButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(builder: (BuildContext context) {
-                      return const CowellMenu();
-                    }),
-                  );
-                },
-                icon: Image.asset('images/crown.png'),
-                iconSize: hallSize,
-              ),
-            ],
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (BuildContext context) {
+                  return const CowellMenu();
+                }),
+              );
+            },
+            icon: Image.asset('images/cowell.png'),
+            iconSize: hallSize,
           ),
-          Column(
-            children: [
-              IconButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(builder: (BuildContext context) {
-                      return const CowellMenu();
-                    }),
-                  );
-                },
-                icon: Image.asset('images/nine.png'),
-                iconSize: hallSize,
-              ),
-              IconButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(builder: (BuildContext context) {
-                      return const CowellMenu();
-                    }),
-                  );
-                },
-                icon: Image.asset('images/carson.png'),
-                iconSize: hallSize,
-              ),
-              IconButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(builder: (BuildContext context) {
-                      return const CowellMenu();
-                    }),
-                  );
-                },
-                icon: Image.asset('images/all.png'),
-                iconSize: hallSize,
-              ),
-            ],
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (BuildContext context) {
+                  return const CowellMenu();
+                }),
+              );
+            },
+            icon: Image.asset('images/porter.png'),
+            iconSize: hallSize,
+          ),
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (BuildContext context) {
+                  return const CowellMenu();
+                }),
+              );
+            },
+            icon: Image.asset('images/crown.png'),
+            iconSize: hallSize,
+          ),
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (BuildContext context) {
+                  return const CowellMenu();
+                }),
+              );
+            },
+            icon: Image.asset('images/nine.png'),
+            iconSize: hallSize,
+          ),
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (BuildContext context) {
+                  return const CowellMenu();
+                }),
+              );
+            },
+            icon: Image.asset('images/carson.png'),
+            iconSize: hallSize,
+          ),
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (BuildContext context) {
+                  return const CowellMenu();
+                }),
+              );
+            },
+            icon: Image.asset('images/all.png'),
+            iconSize: hallSize,
           ),
         ],
       ),
+      // Row(
+      //   mainAxisSize: MainAxisSize.max,
+      //   mainAxisAlignment: MainAxisAlignment.center,
+      //   children: [
+      //     Column(
+      //       children: [
+      //         IconButton(
+      //           onPressed: () {
+      //             Navigator.of(context).push(
+      //               MaterialPageRoute(builder: (BuildContext context) {
+      //                 return const CowellMenu();
+      //               }),
+      //             );
+      //           },
+      //           icon: Image.asset('images/cowell.png'),
+      //           iconSize: hallSize,
+      //         ),
+      //         IconButton(
+      //           onPressed: () {
+      //             Navigator.of(context).push(
+      //               MaterialPageRoute(builder: (BuildContext context) {
+      //                 return const CowellMenu();
+      //               }),
+      //             );
+      //           },
+      //           icon: Image.asset('images/porter.png'),
+      //           iconSize: hallSize,
+      //         ),
+      //         IconButton(
+      //           onPressed: () {
+      //             Navigator.of(context).push(
+      //               MaterialPageRoute(builder: (BuildContext context) {
+      //                 return const CowellMenu();
+      //               }),
+      //             );
+      //           },
+      //           icon: Image.asset('images/crown.png'),
+      //           iconSize: hallSize,
+      //         ),
+      //       ],
+      //     ),
+      //     Column(
+      //       children: [
+      //         IconButton(
+      //           onPressed: () {
+      //             Navigator.of(context).push(
+      //               MaterialPageRoute(builder: (BuildContext context) {
+      //                 return const CowellMenu();
+      //               }),
+      //             );
+      //           },
+      //           icon: Image.asset('images/nine.png'),
+      //           iconSize: hallSize,
+      //         ),
+      //         IconButton(
+      //           onPressed: () {
+      //             Navigator.of(context).push(
+      //               MaterialPageRoute(builder: (BuildContext context) {
+      //                 return const CowellMenu();
+      //               }),
+      //             );
+      //           },
+      //           icon: Image.asset('images/carson.png'),
+      //           iconSize: hallSize,
+      //         ),
+      //         IconButton(
+      //           onPressed: () {
+      //             Navigator.of(context).push(
+      //               MaterialPageRoute(builder: (BuildContext context) {
+      //                 return const CowellMenu();
+      //               }),
+      //             );
+      //           },
+      //           icon: Image.asset('images/all.png'),
+      //           iconSize: hallSize,
+      //         ),
+      //       ],
+      //     ),
+      //   ],
+      // ),
     );
   }
 }

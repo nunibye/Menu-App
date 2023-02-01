6526a51a88fb27b9a744c616a287ef8c4f46ae6f
nunibye <38873297+nunibye@users.noreply.github.com>
2023-01-31 23:18:54 -0800

    doesn't work

--- menu_app/lib/main.dart
+++ menu_app/lib/main.dart
@@ -1,20 +1,67 @@
 import 'dart:async';
 import 'dart:convert';
+import 'dart:ffi';
 
 import 'package:flutter/material.dart';
 import 'constants.dart' as constants;
 import 'package:menu_app/cowell_menu.dart';
 import 'package:menu_app/merrill_menu.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+//import 'package:firebase_core/firebase_core.dart';
+//import 'firebase_options.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(const MyApp());
+import 'package:http/http.dart' as http;
+
+// Future<String> getJsonFromFirebaseRestAPI() async {
+//   String url = "https://ucsc-menu-app-default-rtdb.firebaseio.com/Cowell.json";
+//   http.Response response = await http.get(Uri.parse(url)).once;
+//   return response.body;
+// }
+
+// Future<void> getMenu() async {
+//   final String menuData = await getJsonFromFirebaseRestAPI();
+// }
+
+// class Json {
+//   var menuData = getJsonFromFirebaseRestAPI().;
+// }
+Future<List<Album>> fetchAlbum() async {
+  final response = await http.get(Uri.parse(
+      'https://ucsc-menu-app-default-rtdb.firebaseio.com/Cowell/Lunch/Bakery.json'));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    List<dynamic> data = jsonDecode(response.body);
+    List<Album> list = [];
+    list = data.map((item) => Album.fromJson(item)).toList();
+    return list;
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
+  }
 }
+
+class Album {
+  final String bakery;
+
+  const Album({
+    required this.bakery,
+  });
+
+  factory Album.fromJson(Map<String, dynamic> json) {
+    return Album(
+      bakery: json['bakery'],
+    );
+  }
+}
+
+void main() => runApp(const MyApp());
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp(
+//     options: DefaultFirebaseOptions.currentPlatform,
+//   );
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -45,8 +92,14 @@ class RootPage extends StatefulWidget {
 // }
 
 class _RootPageState extends State<RootPage> {
-  late Future futureAlbum;
+  int currentPageIndex = 0;
+  late Future<List<Album>> futureAlbum;
   @override
+  void initState() {
+    super.initState();
+    futureAlbum = fetchAlbum();
+  }
+
   @override
   Widget build(BuildContext context) {
     double iconSizeCollege = MediaQuery.of(context).size.height / 6;
@@ -153,7 +206,31 @@ class _RootPageState extends State<RootPage> {
                 ],
               ),
             ),
-            Container()
+            Container(
+              alignment: Alignment.topLeft,
+              padding: const EdgeInsets.only(top: 20, left: 12),
+              child: FutureBuilder<List<Album>>(
+                future: futureAlbum,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Text(
+                      snapshot.data.toString(),
+                      style: const TextStyle(
+                          fontSize: 25, color: Color(constants.yellowGold)),
+                    );
+                  } else if (snapshot.hasError) {
+                    return Text(
+                      '${snapshot.error}',
+                      style: const TextStyle(
+                          fontSize: 25, color: Color(constants.yellowGold)),
+                    );
+                  }
+
+                  // By default, show a loading spinner.
+                  return const CircularProgressIndicator();
+                },
+              ),
+            )
           ],
         )
 

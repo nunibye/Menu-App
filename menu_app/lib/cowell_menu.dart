8a399f7a01497e2635234b88f66ebd0139fab11a
christianknab <christiantknab@gmail.com>
2023-02-06 23:47:40 -0800

    added all dining hall hours

--- menu_app/lib/cowell_menu.dart
+++ menu_app/lib/cowell_menu.dart
@@ -107,21 +107,105 @@ class _CowellMenuState extends State<CowellMenu> with TickerProviderStateMixin {
     );
   }
 
+  // void _timeModalBottom(context) {
+  //   showModalBottomSheet(
+  //       context: context,
+  //       shape: const RoundedRectangleBorder(
+  //         borderRadius: BorderRadius.only(
+  //             topLeft: Radius.circular(50), topRight: Radius.circular(50)),
+  //       ),
+  //       builder: (BuildContext bc) {
+  //         return const SizedBox(
+
+  //           height: 800,
+  //           child: Text(
+  //             "Hours",
+  //             textAlign: TextAlign.center,
+  //             style: TextStyle(
+  //               fontFamily: constants.bodyFont,
+  //               fontWeight: FontWeight.w900,
+  //               fontSize: constants.titleFontSize,
+  //               color: Colors.black,
+  //               height: constants.bodyFontheight,
+  //             ),
+  //           ),
+
+  //       );
+  //       });
+  // }
+
   void _timeModalBottom(context) {
     showModalBottomSheet(
-        context: context,
         shape: const RoundedRectangleBorder(
-     borderRadius: BorderRadius.only(topLeft: Radius.circular(50), topRight: Radius.circular(50)),
-  ),
-        builder: (BuildContext bc) {
-          return const SizedBox(height: 800 ,child :Text(
-            "Cowell",
-            style: TextStyle(
-                fontWeight: FontWeight.normal,
-                fontSize: constants.menuHeadingSize,
-                fontFamily: 'Monoton',
-                color: Color(constants.yellowGold)),
-          ));
-        });
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(50), topRight: Radius.circular(50)),
+        ),
+        context: context,
+        builder: (context) => DraggableScrollableSheet(
+              expand: false,
+              builder: (context, scrollController) => SingleChildScrollView(
+                controller: scrollController,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 10, bottom: 30),
+                  child: Column(
+                    children: const [Padding(
+                        padding: EdgeInsets.all(10),
+                        child: Text("Monday-Thursday",
+                            style: TextStyle(
+                              fontFamily: constants.bodyFont,
+                              fontWeight: FontWeight.bold,
+                              fontSize: constants.titleFontSize - 5,
+                              color: Colors.black,
+                              height: constants.bodyFontheight,
+                            ))),
+                      Text(
+                        "Breakfast: 7-11AM\nContinuous Dining: 11-11:30AM\nLunch: 11:30AM-2PM\nContinuous Dining: 2-5PM\nDinner: 5-8PM\nLate Night: 8-11PM",
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(10),
+                        child: Text("Friday",
+                            style: TextStyle(
+                              fontFamily: constants.bodyFont,
+                              fontWeight: FontWeight.bold,
+                              fontSize: constants.titleFontSize - 5,
+                              color: Colors.black,
+                              height: constants.bodyFontheight,
+                            )),
+                      ),
+                      Text(
+                          "Breakfast: 7-11AM\nContinuous Dining: 11-11:30AM\nLunch: 11:30AM-2PM\nContinuous Dining: 2-5PM\nDinner: 5-8PM"),
+                      Padding(
+                        padding: EdgeInsets.all(10),
+                        child: Text("Saturday",
+                            style: TextStyle(
+                              fontFamily: constants.bodyFont,
+                              fontWeight: FontWeight.bold,
+                              fontSize: constants.titleFontSize - 5,
+                              color: Colors.black,
+                              height: constants.bodyFontheight,
+                            )),
+                      ),
+                      Text(
+                        "Breakfast: 7-10AM\nBrunch: 10AM-2PM\nContinuous Dining: 2-5PM\nDinner: 5-8PM",
+                      textAlign: TextAlign.left,),
+                      Padding(
+                        padding: EdgeInsets.all(10),
+                        child: Text("Sunday",
+                            style: TextStyle(
+                              fontFamily: constants.bodyFont,
+                              fontWeight: FontWeight.bold,
+                              fontSize: constants.titleFontSize - 5,
+                              color: Colors.black,
+                              height: constants.bodyFontheight,
+                            )),
+                      ),
+                      Text(
+                        "Breakfast: 7-10AM\nBrunch: 10AM-2PM\nContinuous Dining: 2-5PM\nDinner: 5-8PM\nLate Night: 8-11PM",
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ));
   }
 }

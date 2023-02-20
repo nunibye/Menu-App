858ac21ecb75e027f8637ef2db27aa3837d38bf4
christianknab <christiantknab@gmail.com>
2023-02-19 22:45:58 -0800

    finds days between dates

--- menu_app/lib/calculator.dart
+++ menu_app/lib/calculator.dart
@@ -15,20 +15,20 @@ class Calculator extends StatefulWidget {
 
 class _CalculatorPageState extends State<Calculator> {
   static const totalSlugPoints = 1000.0;
-  static const mealDay = 3;
+  static const mealDay = 3.0;
   // var lastDay = "3 / 24 / 23"; FIXME: should be a date
-  static const lastDay = 10; // FIXME: Right now only how many days left
+  // static const lastDay = 10; // FIXME: Right now only how many days left
   static const mealCost = 8.28;
 
   final _totalSlugPointsController =
       TextEditingController(text: totalSlugPoints.toString());
   final _mealDayController = TextEditingController(text: mealDay.toString());
-  final _lastDayController = TextEditingController(text: lastDay.toString());
+  // final _lastDayController = TextEditingController(text: lastDay.toString());
   TextEditingController dateController = TextEditingController();
 
   double _totalSlugPoints = totalSlugPoints;
-  int _mealDay = mealDay;
-  int _lastDay = lastDay;
+  double _mealDay = mealDay;
+  // int _lastDay = lastDay;
   // String _lastDay = lastDay; FIXME
 
   @override
@@ -36,8 +36,8 @@ class _CalculatorPageState extends State<Calculator> {
     super.initState();
     _totalSlugPointsController.addListener(_onTotalSlugPointsChanged);
     _mealDayController.addListener(_onMealDayChanged);
-    _lastDayController.addListener(_onLastDayChanged);
-    dateController.text = "2023-02-20";
+    // _lastDayController.addListener(_onLastDayChanged);
+    dateController.text = "2023-02-21";
   }
 
   _onTotalSlugPointsChanged() {
@@ -49,21 +49,21 @@ class _CalculatorPageState extends State<Calculator> {
 
   _onMealDayChanged() {
     setState(() {
-      _mealDay = int.tryParse(_mealDayController.text) ?? 0;
+      _mealDay = double.tryParse(_mealDayController.text) ?? 0;
     });
   }
 
-  _onLastDayChanged() {
-    setState(() {
-      _lastDay = int.tryParse(_lastDayController.text) ?? 0;
-    });
-  }
+  // _onLastDayChanged() {
+  //   setState(() {
+  //     _lastDay = int.tryParse(_lastDayController.text) ?? 0;
+  //   });
+  // }
 
   @override
   void dispose() {
     // To make sure we are not leaking anything, dispose any used TextEditingController
     // when this widget is cleared from memory.
-    _lastDayController.dispose();
+    // _lastDayController.dispose();
     _mealDayController.dispose();
     _totalSlugPointsController.dispose();
     super.dispose();
@@ -71,11 +71,15 @@ class _CalculatorPageState extends State<Calculator> {
 
   @override
   Widget build(BuildContext context) {
+    getDays() => num.parse(
+        (DateTime.parse(dateController.text).difference(DateTime.now()).inDays + 1)
+            .toString());
+
     getMealAmount() => num.parse(
-        ((_totalSlugPoints - (_mealDay * mealCost * _lastDay)) / mealCost)
+        ((_totalSlugPoints - (_mealDay * mealCost * getDays())) / mealCost)
             .toStringAsFixed(2));
     getPointsAmount() =>
-        num.parse(((_totalSlugPoints - (_mealDay * mealCost * _lastDay)))
+        num.parse(((_totalSlugPoints - (_mealDay * mealCost * getDays())))
             .toStringAsFixed(2));
 
     return Scaffold(
@@ -241,7 +245,7 @@ class _CalculatorPageState extends State<Calculator> {
                   child: Column(
                     children: [
                       Text(
-                        'Remaining Meals: ${getMealAmount()}\nSlug Points: ${getPointsAmount()}',
+                        'Days Left: ${getDays()}\nRemaining Meals: ${getMealAmount()}\nSlug Points: ${getPointsAmount()}',
                         key: const Key('mealAmount'),
                         style: const TextStyle(
                             color: Color(constants.bodyColor),
@@ -260,23 +264,23 @@ class _CalculatorPageState extends State<Calculator> {
   }
 }
 
-class AmountText extends StatelessWidget {
-  final String text;
+// class AmountText extends StatelessWidget {
+//   final String text;
 
-  const AmountText(
-    this.text, {
-    required Key key,
-  }) : super(key: key);
+//   const AmountText(
+//     this.text, {
+//     required Key key,
+//   }) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(8),
-      child: Text(text.toUpperCase(),
-          style: const TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Color(constants.bodyColor),
-              fontSize: 20)),
-    );
-  }
-}
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Container(
+  //     padding: const EdgeInsets.all(8),
+  //     child: Text(text.toUpperCase(),
+  //         style: const TextStyle(
+  //             fontWeight: FontWeight.bold,
+  //             color: Color(constants.bodyColor),
+  //             fontSize: 20)),
+  //   );
+  // }
+// }

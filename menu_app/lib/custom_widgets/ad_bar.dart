d518c2ba77f23acfd3d58c1ab3d2a55615c3dcac
christianknab <christiantknab@gmail.com>
2024-01-06 22:32:51 -0800

    fix ad bug

--- menu_app/lib/custom_widgets/ad_bar.dart
+++ menu_app/lib/custom_widgets/ad_bar.dart
@@ -1,62 +1,17 @@
-import 'package:applovin_max/applovin_max.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:menu_app/custom_widgets/controllers/ad_bar_controller.dart';
-import 'package:menu_app/utilities/ad_helper.dart' as ad_helper;
-import 'package:provider/provider.dart';
-
-class AdBar extends StatelessWidget {
-  const AdBar({Key? key});
-
-  @override
-  Widget build(BuildContext context) {
-    var viewModel = Provider.of<AdBarController>(context);
-
-    if (viewModel.showAd) {
-      return Container(
-        //color: Colors.amber,
-        alignment: Alignment.topCenter,
-        height: viewModel.adHeight,
-        child: viewModel.showAd
-            ? MaxAdView(
-                adUnitId: ad_helper.getAdUnitId,
-                adFormat: AdFormat.banner,
-                listener: AdViewAdListener(
-                  onAdLoadedCallback: (ad) {
-                    viewModel.loadAd();
-                  },
-                  onAdLoadFailedCallback: (adUnitId, error) {
-                    viewModel.loadAd();
-                  },
-                  onAdClickedCallback: (ad) {},
-                  onAdExpandedCallback: (ad) {},
-                  onAdCollapsedCallback: (ad) {},
-                ),
-              )
-            : null,
-      );
-    } else {
-      return const SizedBox(width: 0, height: 0);
-    }
-  }
-}
+// import 'package:applovin_max/applovin_max.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter/widgets.dart';
+// import 'package:menu_app/custom_widgets/controllers/ad_bar_controller.dart';
+// import 'package:menu_app/utilities/ad_helper.dart' as ad_helper;
+// import 'package:provider/provider.dart';
 
 // class AdBar extends StatelessWidget {
-//   const AdBar({super.key});
+//   const AdBar({Key? key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return ChangeNotifierProvider(
-//       create: (_) => AdBarController(),
-//       child: _AdBar(),
-//     );
-//   }
-// }
-
-// class _AdBar extends StatelessWidget {
 //   @override
 //   Widget build(BuildContext context) {
 //     var viewModel = Provider.of<AdBarController>(context);
+
 //     if (viewModel.showAd) {
 //       return Container(
 //         //color: Colors.amber,
@@ -81,10 +36,110 @@ class AdBar extends StatelessWidget {
 //             : null,
 //       );
 //     } else {
-//       return const SizedBox(
-//         width: 0,
-//         height: 0,
-//       );
+//       return const SizedBox(width: 0, height: 0);
 //     }
 //   }
 // }
+
+// // class AdBar extends StatelessWidget {
+// //   const AdBar({super.key});
+
+// //   @override
+// //   Widget build(BuildContext context) {
+// //     return ChangeNotifierProvider(
+// //       create: (_) => AdBarController(),
+// //       child: _AdBar(),
+// //     );
+// //   }
+// // }
+
+// // class _AdBar extends StatelessWidget {
+// //   @override
+// //   Widget build(BuildContext context) {
+// //     var viewModel = Provider.of<AdBarController>(context);
+// //     if (viewModel.showAd) {
+// //       return Container(
+// //         //color: Colors.amber,
+// //         alignment: Alignment.topCenter,
+// //         height: viewModel.adHeight,
+// //         child: viewModel.showAd
+// //             ? MaxAdView(
+// //                 adUnitId: ad_helper.getAdUnitId,
+// //                 adFormat: AdFormat.banner,
+// //                 listener: AdViewAdListener(
+// //                   onAdLoadedCallback: (ad) {
+// //                     viewModel.loadAd();
+// //                   },
+// //                   onAdLoadFailedCallback: (adUnitId, error) {
+// //                     viewModel.loadAd();
+// //                   },
+// //                   onAdClickedCallback: (ad) {},
+// //                   onAdExpandedCallback: (ad) {},
+// //                   onAdCollapsedCallback: (ad) {},
+// //                 ),
+// //               )
+// //             : null,
+// //       );
+// //     } else {
+// //       return const SizedBox(
+// //         width: 0,
+// //         height: 0,
+// //       );
+// //     }
+// //   }
+// // }
+import 'package:applovin_max/applovin_max.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:menu_app/custom_widgets/controllers/ad_bar_controller.dart';
+import 'package:menu_app/utilities/ad_helper.dart' as ad_helper;
+import 'package:provider/provider.dart';
+
+class AdBar extends StatelessWidget {
+  const AdBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => AdBarController(),
+      child: _AdBar(),
+    );
+  }
+}
+
+class _AdBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var viewModel = Provider.of<AdBarController>(context, listen: true);
+    print(viewModel.showAd);
+    if (viewModel.showAd) {
+      return Container(
+        //color: Colors.amber,
+        alignment: Alignment.topCenter,
+        height: viewModel.adHeight,
+        child: viewModel.showAd
+            ? MaxAdView(
+                adUnitId: ad_helper.getAdUnitId,
+                adFormat: AdFormat.banner,
+                listener: AdViewAdListener(
+                  onAdLoadedCallback: (ad) {
+                    viewModel.loadAd();
+                  },
+                  onAdLoadFailedCallback: (adUnitId, error) {
+                    viewModel.loadAd();
+                  },
+                  onAdClickedCallback: (ad) {},
+                  onAdExpandedCallback: (ad) {},
+                  onAdCollapsedCallback: (ad) {},
+                ),
+              )
+            : null,
+      );
+    } else {
+      return const SizedBox(
+        width: 0,
+        height: 0,
+      );
+    }
+  }
+}
--- menu_app/lib/main.dart
+++ menu_app/lib/main.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:menu_app/custom_widgets/controllers/ad_bar_controller.dart';
+import 'package:menu_app/models/ads.dart';
 import 'package:menu_app/utilities/router.dart';
 import 'package:provider/provider.dart';
 import 'utilities/constants.dart' as constants;
@@ -12,6 +13,7 @@ import 'utilities/firebase_options.dart';
 // MAIN function sets preferences.
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await getAdBool();
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
@@ -29,27 +31,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider<AdBarController>(create: (_) => AdBarController()),
-        // Add more providers as needed
-      ],
-      child: MaterialApp.router(
-        debugShowCheckedModeBanner: false,
-        // Ignores IOS set to bold text
-        builder: (context, child) => MediaQuery(
-          data: MediaQuery.of(context).copyWith(boldText: false),
-          child: child!,
-        ),
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: constants.darkThemeColors(context),
-          buttonTheme: const ButtonThemeData(
-            colorScheme: ColorScheme.dark(),
-          ),
-        ),
-        routerConfig: goRouter,
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      // Ignores IOS set to bold text
+      builder: (context, child) => MediaQuery(
+        data: MediaQuery.of(context).copyWith(boldText: false),
+        child: child!,
       ),
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: constants.darkThemeColors(context),
+        buttonTheme: const ButtonThemeData(
+          colorScheme: ColorScheme.dark(),
+        ),
+      ),
+      routerConfig: goRouter,
     );
   }
 }
